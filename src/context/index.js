631ba90d8bc532c0b0a7dee6ce354078d3d4fb0d
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-05-29 09:35:00 +0500

    Counter

--- src/context/index.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-
-export const dataContext = createContext(null)
-
-const {Provider} = dataContext
-
-export default Provider
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/app'
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
